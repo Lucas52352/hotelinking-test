182ff0be0963f9a7662e8936a7d8db184d592dce
Lucas52352 <lucasrius86@gmail.com>
2024-10-17 18:09:34 -0300

    configure .env

--- hotelinking-test-frontend/src/services/axiosInstance.ts
+++ hotelinking-test-frontend/src/services/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: 'http://127.0.0.1:8000',
   headers: {
     "X-Requested-With": "XMLHttpRequest",
     "Accept": "application/json",
